e59066d27181784e7fedd3e9b508073bd92d73e8
sunandadhingra <sunanda.dhingra87@gmail.com>
2016-05-12 17:38:44 +0530

    UI changes

--- project/esdemo/src/main/resources/static/JS/ElasticDemo.js
+++ project/esdemo/src/main/resources/static/JS/ElasticDemo.js
@@ -1,14 +1,22 @@
-function callElastic(){
-var textbox=document.getElementById('globalSearchBox');
-//var searchBy=document.getElementById('searchBy').value;
-var searchBy= document.querySelector('input[name="searchBy"]:checked').value
-var url= "search/"+searchBy+"?text="+textbox.value;
-makeAjaxGETCall(url,"searchResult");
-
+var globalSearchBox= document.getElementById('globalSearchBox');
+globalSearchBox.onkeyup = function() {
+	if(globalSearchBox.value.length>=4){
+		makeAjaxGETCall("suggest?text="+globalSearchBox.value,"searchResultDatalist");	
+	}
+	else if(globalSearchBox.value.length<4){
+		document.getElementById("searchResultDatalist").innerHTML='';
+	}	
 }
-
-function makeAjaxGETCall(url,id){
-	
+function callElastic(){
+var searchBy= document.querySelector('input[name="searchBy"]:checked').value
+var url= "search?text="+globalSearchBox.value+"&type="+searchBy;
+makeAjaxGETCall(url,"searchResultTable");
+}
+function callElastic1(companycode){
+var url= "search/employeesbycompany?text="+companycode+"&type=employee";
+makeAjaxGETCall(url,"showEmployeesContent");
+}
+function makeAjaxGETCall(url,id){	
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
@@ -18,5 +26,4 @@ function makeAjaxGETCall(url,id){
   };
   xhttp.open("GET", url+"&t=" + Math.random(), true);
   xhttp.send();
-
 }
